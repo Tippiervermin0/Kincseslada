12a717908e056b5d3f720f356fe6bf3150107018
hallgato hallgato <hallgato@oktatas.local>
2021-09-10 16:42:56 +0200

    osztály és main kialakítása

--- src/kincseslada/Kincseslada.java
+++ src/kincseslada/Kincseslada.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kincseslada;
 
-/**
- *
- * @author hallgato
- */
-public class Kincseslada {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Kincseslada {
+    
+    static Scanner sc = new Scanner(System.in);
+    static Ladak a = new Ladak("Arany","Bennem van a kincs",0);
+    static Ladak e = new Ladak("Ezüst","Nem bennem van a kincs",1);
+    static Ladak b = new Ladak("Bronz","Az arany hazudik",0);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Start();
+        
     }
+    
+    public static void Start(){
+        System.out.println(a.toString());
+        System.out.println(e.toString());
+        System.out.println(b.toString());
+        
+        
+        
+        System.out.println("Hanyadik ládában van a kincs:");
+        int melyik = sc.nextInt();
+        while(melyik < 1 || melyik > 3){
+            System.out.println("Hanyadik ládában van a kincs:");
+            melyik = sc.nextInt();
+        }
+        if(melyik == 2){
+            System.out.println("Eltaláltad!");
+        }else{
+            System.out.println("Nem talált!");
+        }
+    }
+   
+    
+
     
 }
--- src/kincseslada/Ladak.java
+++ src/kincseslada/Ladak.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kincseslada;
 
-/**
- *
- * @author hallgato
- */
+
 public class Ladak {
+    private String nev;
+    private String felirat;
+    private int kincs;
+    
+    Ladak(String nev,String felirat, int kincs){
+        this.nev=nev;
+        this.felirat=felirat;
+        this.kincs=kincs;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public String getFelirat() {
+        return felirat;
+    }
+
+    public int getKincs() {
+        return kincs;
+    }
+    
+    @Override
+    public String toString(){
+        return getNev()+":"+ getFelirat();
+    }
+    
     
 }
